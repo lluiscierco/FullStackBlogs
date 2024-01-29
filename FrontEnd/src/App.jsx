2da2cee321cefdee6d7c295fc976e8966608a9bd
lluis <63962612+lluiscierco@users.noreply.github.com>
2024-01-29 12:51:58 +0000

    create togglable component and extract newBlog to its own component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import NewBlog from "./components/NewBlog";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -125,28 +127,17 @@ const App = () => {
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
-      <h2>Create new blog</h2>
-      <form onSubmit={handleBlogSubmit}>
-        <div>
-          <label htmlFor="title">Title: </label>
-          <input
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="Author">Author: </label>
-          <input
-            value={author}
-            onChange={(event) => setAuthor(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="Url">Url: </label>
-          <input value={url} onChange={(event) => setUrl(event.target.value)} />
-        </div>
-        <button type="Submit">Post</button>
-      </form>
+      <Togglable buttonLabel="Create new blog">
+        <NewBlog
+          handleBlogSubmit={handleBlogSubmit}
+          title={title}
+          setTitle={setTitle}
+          author={author}
+          setAuthor={setAuthor}
+          url={url}
+          setUrl={setUrl}
+        />
+      </Togglable>
     </div>
   );
 };
---
+++ FrontEnd/src/components/NewBlog.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+const NewBlog = ({
+  handleBlogSubmit,
+  title,
+  setTitle,
+  author,
+  setAuthor,
+  url,
+  setUrl,
+}) => {
+  return (
+    <div>
+      <h2>Create new blog</h2>
+      <form onSubmit={handleBlogSubmit}>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="Author">Author: </label>
+          <input
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="Url">Url: </label>
+          <input value={url} onChange={(event) => setUrl(event.target.value)} />
+        </div>
+        <button type="Submit">Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewBlog;
